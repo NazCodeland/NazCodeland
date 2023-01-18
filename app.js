ce4558d658ecc2cca7cf692f64326f395d26f6fb
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-01-17 22:07:12 -0500

    install moduled winston, morgan, vitest, livereload, connect-livereload, favicon. Implemented Hot Module Reload middleware code in app.js and to handle favicon requests

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/extensions */
 import express from 'express';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { engine } from 'express-handlebars';
 
+// -----------------------------------------------------
+// route imports
+// -----------------------------------------------------
+
+// -----------------------------------------------------
+// middleware imports
+// -----------------------------------------------------
+
+import livereload from 'livereload';
+import connectLiveReload from 'connect-livereload';
+import favicon from 'serve-favicon';
 import homePageRoutes from './src/routes/homePageRoute.js';
 
-const app = express();
+// -----------------------------------------------------
+// hot module reload for browser
+// -----------------------------------------------------
+// setting up livereload and connect-livereload modules
+// so that the browser refreshes whenever nodemon restarts the server
+// on changes to any files within the whole project directory
+// https://dev.to/cassiolacerda/automatically-refresh-the-browser-on-node-express-server-changes-x1f680-1k0o#:~:text=Create%20a%20Livereload%20server%20and,refresh(%22%2F%22)%3B%20.
+const liveReloadServer = livereload.createServer();
+
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 10);
+});
 
 // -----------------------------------------------------
 // template engine settings
 // -----------------------------------------------------
+const app = express();
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App URL: http://www.localhost:3000`);
+});
 
 app.engine('.hbs', engine({ extname: '.hbs', helpers: {} }));
 app.set('view engine', '.hbs');
 app.set('views', 'src/views');
 
 // -----------------------------------------------------
-// express middleware
+// middleware usage
 // -----------------------------------------------------
+// for adding the Livereload script to the response.
+app.use(connectLiveReload());
 
 app.use(express.static('src/public', { extensions: ['.js'] }));
-// app.use(express.json());
-// app.use(express.urlencoded());
 
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App URL: http://www.localhost:3000`);
-});
+app.use(favicon('src/public/images/favicon.png'));
 
 app.use(homePageRoutes);
