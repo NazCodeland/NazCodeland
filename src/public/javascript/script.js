c369a795992a766fd6a068535e2cf4b8ec043052
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-01-20 13:24:05 -0500

    changed navigation default shadow, hover state shadow, active state shadow

--- src/public/javascript/script.js
+++ src/public/javascript/script.js
@@ -1,11 +1,4 @@
 /* eslint-disable import/extensions */
 import './utils/spanWord.js';
 import './components/skillCard.js';
-
-const menuLinks = document.querySelectorAll('.menu li');
-
-function handleClick(event) {
-  // event.preventDefault();
-}
-
-menuLinks.forEach((link) => link.addEventListener('click', handleClick));
+import './sections/navigation.js';
