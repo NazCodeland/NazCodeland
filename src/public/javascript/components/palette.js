7e9c33d67c84b212af38c7a91690534c712fcbab
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-02-13 15:02:29 -0500

    added javascript logic for the palette buttons to change the custom css properties impacting the theme and contrast themes of the whole site.

--- src/public/javascript/components/palette.js
+++ src/public/javascript/components/palette.js
@@ -1,8 +1,31 @@
+const root = document.querySelector(':root');
+
 const menuBtn = document.querySelector('.hamburger');
-const palettes = document.querySelector('.palettes');
+const palettesContainer = document.querySelector('.palettes');
+const palettes = document.querySelectorAll('.palette');
 
 function showPalettes(event) {
-  palettes.classList.toggle('show-palettes');
+  palettesContainer.classList.toggle('show-palettes');
+}
+menuBtn.addEventListener('click', showPalettes);
+
+function getCurrentPalette(event) {
+  const currentPalette = localStorage.getItem('NazCodeland.palette');
+  return currentPalette;
 }
 
-menuBtn.addEventListener('click', showPalettes);
+function setPalette(chosenPalette) {
+  root.setAttribute('theme', chosenPalette);
+  localStorage.setItem('NazCodeland.palette', chosenPalette);
+}
+
+palettes.forEach((palette) =>
+  palette.addEventListener('click', (event) => {
+    const chosenPalette = event.target.name;
+    setPalette(chosenPalette);
+  })
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  setPalette(getCurrentPalette());
+});
